1ea888ec9279ac1e47df6948bf5e2b6b5cc932ac
Srikar Rao Gandla <srikar.sirks@gmail.com>
2019-09-19 21:47:05 -0700

    Added brute force approach using dfs

--- epi-java/src/com/eop/java/programs/dynamicprogramming/Traversing2DArray.java
+++ epi-java/src/com/eop/java/programs/dynamicprogramming/Traversing2DArray.java
@@ -1,4 +1,4 @@
-package com.eop.java.programs.dynamicprogramming;
+package com.programs.poc.com.eop.java.programs.dynamicprogramming;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,32 +6,71 @@ import java.util.List;
 
 /**
  * JAVA program to find the number of ways to traverse a 2D array
- * 
- * @author SrikarRao
  *
+ * @author SrikarRao
  */
 public class Traversing2DArray {
 
-	public static int numberOfWays(int n, int m) {
+    private static class Counter {
+        int count;
+    }
 
-		if (n <= 0 || m <= 0) {
-			return -1;
-		}
+    public static int numberOfWays(int n, int m) {
 
-		if (n < m) {
-			n = n + m;
-			m = n - m;
-			n = n - m;
-		}
+        if (n <= 0 || m <= 0) {
+            return -1;
+        }
 
-		List<Integer> A = new ArrayList<Integer>(Collections.nCopies(m, 1));
-		for (int i = 1; i < n; i++) {
-			int prevRes = 0;
-			for (int j = 0; j < m; j++) {
-				A.set(j, A.get(j) + prevRes);
-				prevRes = A.get(j);
-			}
-		}
-		return A.get(m - 1);
-	}
+        if (n < m) {
+            n = n + m;
+            m = n - m;
+            n = n - m;
+        }
+
+        List<Integer> A = new ArrayList<Integer>(Collections.nCopies(m, 1));
+        for (int i = 1; i < n; i++) {
+            int prevRes = 0;
+            for (int j = 0; j < m; j++) {
+                A.set(j, A.get(j) + prevRes);
+                prevRes = A.get(j);
+            }
+        }
+        return A.get(m - 1);
+    }
+
+
+    public static int numberOfWaysBruteForce(int n, int m) {
+        Counter counter = new Counter();
+        dfs(0, 0, n, m, counter);
+        return counter.count;
+    }
+
+    private static void dfs(int i, int j, int n, int m, Counter counter) {
+
+        if (i >= n || j >= m) {
+            return;
+        }
+
+        if (i == n - 1 && j == m - 1) {
+            counter.count++;
+            return;
+        }
+
+        dfs(i + 1, j, n, m, counter);
+        dfs(i, j + 1, n, m, counter);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfWays(3, 3) + ", " + numberOfWaysBruteForce(3, 3));
+
+        System.out.println(numberOfWays(2, 3) + ", " + numberOfWaysBruteForce(2, 3));
+
+        System.out.println(numberOfWays(3, 2) + ", " + numberOfWaysBruteForce(3, 2));
+
+        System.out.println(numberOfWays(5, 5) + ", " + numberOfWaysBruteForce(5, 5));
+
+        System.out.println(numberOfWays(12, 2) + ", " + numberOfWaysBruteForce(12, 2));
+
+        System.out.println(numberOfWays(1, 1) + ", " + numberOfWaysBruteForce(1, 1));
+    }
 }
